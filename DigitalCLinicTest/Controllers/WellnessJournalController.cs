4524d3b9f6e63e4696879b685af97b1a27972d6f
twopact <artemov9321@mail.ru>
2026-02-11 11:25:12 +0300

    Функционал для пациентов:
    Реализован метод для ведения дневника самочувствия для фиксации субъективных ощущений и симптомов

---
+++ DigitalCLinicTest/Controllers/WellnessJournalController.cs
@@ -0,0 +1,47 @@
+﻿using DigitalCLinicTest.DatabaseContext;
+using DigitalCLinicTest.Helpers;
+using DigitalCLinicTest.Models;
+using DigitalCLinicTest.RequestModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigitalCLinicTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WellnessJournalController : ControllerBase
+    {
+        private readonly DigitalTwinPatientDbtestContext _db;
+
+        public WellnessJournalController(DigitalTwinPatientDbtestContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        public IActionResult CreateWellnessJournal([FromBody] CreateWellnessJournalRequestModel model)
+        {
+            try
+            {
+                var wellnessJournal = new WellnessJournal
+                {
+                    PatientId = model.PatientId,
+                    EntryDate = model.EntryDate,
+                    MoodScore = model.MoodScore,
+                    Notes = model.Notes
+                };
+
+                _db.WellnessJournals.Add(wellnessJournal);
+                _db.SaveChanges();
+
+                return Ok(new
+                {
+                    Message = "Дневник самочувствия создан"
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ DigitalCLinicTest/RequestModel/WellnessJournalRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DigitalCLinicTest.RequestModel
+{
+    public class CreateWellnessJournalRequestModel
+    {
+        public int PatientId { get; set; }
+        public DateOnly EntryDate { get; set; }
+        public int? MoodScore { get; set; }
+        public string? Notes { get; set; }
+    }
+}
